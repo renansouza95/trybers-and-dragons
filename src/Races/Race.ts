df702694c1ced9689d771507b9d2b230405e5ef4
renansouza95 <s.renan@outlook.com>
2022-06-07 22:25:18 -0300

    feat: class Archetype

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -7,15 +7,15 @@ abstract class Race {
     this._dexterity = dexterity;
   }
 
+  get name(): string { return this._name; }
+
+  get dexterity(): number { return this._dexterity; }
+
   static createdRacesInstances(): number { 
     throw new Error('Not implemented');
   }
 
   abstract get maxLifePoints(): number;
-
-  get name(): string { return this.name; }
-
-  get dexterity(): number { return this.dexterity; }
 }
 
 export default Race;
