516c23e63c385d978eff53877ca4845e65524062
renansouza95 <s.renan@outlook.com>
2022-06-09 16:58:05 -0300

    feat: creates PVP #vocequelute

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -4,18 +4,20 @@ import Battle from './Battle';
 class PVP extends Battle {
   private _player1: Fighter;
   private _player2: Fighter;
-  private _winner: number;
 
   constructor(player1: Fighter, player2: Fighter) {
     super(player1);
-    this._winner = super.fight();
     this._player1 = player1;
     this._player2 = player2;
   }
 
-  get winner(): Fighter {
-    if (this._winner === 1) { return this._player1; }
-    return this._player2;
+  fight() {
+    super.fight();
+    if (this._player2.strength > this._player1.strength
+      && this._player2.defense > this._player1.defense) {
+      return -1;
+    }
+    return 1;
   }
 }
 
